28454b8d14d5accbf5f18871eb6ad7d9f366ad64
Nuunnzzz <noha_malek@outlook.com>
2020-07-05 18:25:38 +0200

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  title = 'My Todos';
+  todos = [
+    {
+      label: 'React Tasks',
+      done: false,
+    },
+    {
+      label: 'Watch Movie',
+      done: false,
+    },
+    {
+      label: 'Angular Task',
+      done: true,
+    },
+    {
+      label: 'Play my music',
+      done: false,
+
+    },
+    {
+      label: 'Sleep',
+      done: true,
+    }
+    
+  ];
+
+  addTodo(newTodoLabel) {
+    var newTodo = {
+      label: newTodoLabel,
+      done: false
+    };
+    this.todos.push(newTodo)
+  }
+
+  deleteTodo(todo) {
+    this.todos = this.todos.filter( t => t.label !== todo.label)
+  }
 }
